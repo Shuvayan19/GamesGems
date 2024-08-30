e171760ff9139cca1505a16ea367c46ac1846e65
Shuvayan Nandy <sbhnandy2@gmail.com>
2024-08-31 04:09:14 +0530

    Small changes

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL: 'https://free-to-play-games-database.p.rapidapi.com/api',
   headers: {
-    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY || '',
+    'x-rapidapi-key': 'de65aa4b14msh320427167e02ee7p1075f2jsn1db2396dfdaf',
     'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
   },
 });
