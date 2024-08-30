9704dab2f5dc2676b46907394191e9715f996ef9
Shuvayan Nandy <sbhnandy2@gmail.com>
2024-08-31 03:30:27 +0530

    Api fixed

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL:'https://www.freetogame.com/api' 
-})
+const apiClient = axios.create({
+  baseURL: "https://free-to-play-games-database.p.rapidapi.com/api", // Base URL of the API
+  headers: {
+    "x-rapidapi-key": "de65aa4b14msh320427167e02ee7p1075f2jsn1db2396dfdaf",
+    "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+  },
+});
+
+export default apiClient;
