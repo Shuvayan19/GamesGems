f87cef979c6e114ac98965caf6abef7560bd85bd
Shuvayan Nandy <sbhnandy2@gmail.com>
2024-10-27 00:51:55 +0530

    Fixed the fetching issue

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,17 +3,16 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://free-to-play-games-database.p.rapidapi.com/api",
   headers: {
-    "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
-    "x-rapidapi-host": process.env.REACT_APP_RAPIDAPI_HOST,
+    "x-rapidapi-key": import.meta.env.VITE_RAPIDAPI_KEY,
+    "x-rapidapi-host": import.meta.env.VITE_RAPIDAPI_HOST,
   },
 });
 class APIClient<T> {
   endpoint: string;
   constructor(endpoint: string) {
     this.endpoint = endpoint;
-    console.log("API Key:", process.env.NEXT_PUBLIC_RAPIDAPI_KEY);
-    console.log("API Host:", process.env.NEXT_PUBLIC_RAPIDAPI_HOST);
-
+    console.log("API Key:", import.meta.env.VITE_RAPIDAPI_KEY);
+    console.log("API Host:", import.meta.env.VITE_RAPIDAPI_HOST);
   }
   getAll = () => {
     return axiosInstance.get<T[]>(this.endpoint).then((res) => res.data);
