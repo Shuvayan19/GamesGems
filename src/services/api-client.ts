2ffc08782bc0456f1b19a0ba7876b5df00988c7f
Shuvayan Nandy <sbhnandy2@gmail.com>
2024-10-26 23:38:40 +0530

    Added api-client(removed sensetive data)

---
+++ src/services/api-client.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://free-to-play-games-database.p.rapidapi.com/api",
+  headers: {
+    "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPIDAPI_HOST,
+    "x-rapidapi-host": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+  },
+});
+class APIClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  getAll = () => {
+    return axiosInstance.get<T[]>(this.endpoint).then((res) => res.data);
+  };
+}
+
+export default APIClient;
